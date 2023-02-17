aab40b2e2b4b0d7f106ee50086793bf40da388f9
nayana.v <nayana.v@compile.com>
2023-02-18 03:58:27 +0530

    click events (button clicks)

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,33 @@
 const Home = () => {
+
+    // handleMouseOver, handleSubmit: typical function names
+    const handleClick = (e) =>{
+        console.log("heyy!", e);
+    }
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name + e.target); //concatenate
+        console.log(`hello ${name}` ); //template string instead of +
+    }
+
     return ( 
         <div className="home">
             <h1>Home Page</h1>
+            <button onClick={handleClick}>Click me</button>
+            {/* 
+            above code sets a reference to the function, fn is invoked when clicked on the button only
+            this below code with parenthesis invokes/fires the handleClick() function without needing to click on the button
+            <button onClick={handleClick()}>Click me</button> */}
+            
+            {/* automatically passes 'e' parameter = event object on firing */}
+
+            <button onClick={(e) => {
+                handleClickAgain('miya', e)
+            }}>Click me again</button>
+            {/* handleClickAgain does not get an eventobject as handleClickAgain is not being referenced but the anonymous function by ()=>{} is 
+            anonymous function gets access to the event object automatically*/}
+            {/* 
+            <button onClick={() => handleClickAgain('miya') }>Click me again</button>
+            */}
         </div>
      );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,15 +5,7 @@ const Navbar = () => {
         <h1>The React Blog</h1>
         <div className="links">
           <a href="/">Home</a>
-          <a href="/create" style={{
-            // key value pairs
-            color: "white",
-            //not background-color like in css, '-' is minus in JS
-            backgroundColor:  '#f1356d',
-            borderRadius: '8px'
-          }}>New Blog</a>
-          {/* {} single braces for dynamic value */}
-          {/* {{}} double for JS objects as dynamic values */}
+          <a href="/create">New Blog</a>
         </div>
       </nav>
     );
