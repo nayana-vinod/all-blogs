8082a0000edc14a89ba1397072d3e5ec1f267df4
nayana.v <nayana.v@compile.com>
2023-02-18 04:33:11 +0530

    useState hook: making reactive values

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,34 @@
+import { useState } from "react";
+//to make variables reactive 
+// reactive value: something that might change its value at some point
+
 const Home = () => {
+    // let name = 'miya';
 
-    // handleMouseOver, handleSubmit: typical function names
+    // can use the same hook in a component for diff values
+    // datatype of state can be anything, object, boolean, etc
+    const [name, setName] = useState('miya'); //initial value=' miya'
+    //save it in an array destructuring to store two values this hook returns
+    //name: any var for storing initial value 'miya'
+    // setVariable: function to change variable (typical format)
+
+    const [age, setAge] = useState(25);
+
     const handleClick = (e) =>{
-        console.log("heyy!", e);
+        // name = 'louis';
+        // console.log(name); 
+        //without useState hook, the value of name will be changed but
+        // wont be reflected in the page
+        // as after the change, the component is not re-rendreded
+        setName('louis');
+        setAge(30);
     }
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name + e.target); //concatenate
-        console.log(`hello ${name}` ); //template string instead of +
-    }
-
+ 
     return ( 
         <div className="home">
             <h1>Home Page</h1>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click me</button>
-            {/* 
-            above code sets a reference to the function, fn is invoked when clicked on the button only
-            this below code with parenthesis invokes/fires the handleClick() function without needing to click on the button
-            <button onClick={handleClick()}>Click me</button> */}
-            
-            {/* automatically passes 'e' parameter = event object on firing */}
-
-            <button onClick={(e) => {
-                handleClickAgain('miya', e)
-            }}>Click me again</button>
-            {/* handleClickAgain does not get an eventobject as handleClickAgain is not being referenced but the anonymous function by ()=>{} is 
-            anonymous function gets access to the event object automatically*/}
-            {/* 
-            <button onClick={() => handleClickAgain('miya') }>Click me again</button>
-            */}
         </div>
      );
 }
