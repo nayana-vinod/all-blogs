a3177a1e3ec078a103375aa95c45abadfc4a420c
nayana.v <nayana.v@compile.com>
2023-02-18 00:17:14 +0530

    Dynamic Values in JSX Templates in src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // any valid js here
+  const title = 'New Blog' ;
+  const likes = 50;
+  //any type like array, etc except bool and object
+
+   // const person = { name: 'yoshi', age: 30 };
+   const link = 'http://www.google.com';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* className: camelcase */}
+      {/* not 'class' like in html
+      class is a keyword in JS, and this is basically a JS file.
+      babel converts className into class in html for the dom */}
+      <div className="content">
+        {/* <h1>App Component</h1> */}
+        <h1>WELCOME to { title }!</h1>
+        <p>Liked { likes } times</p>
+
+        {/* <p>{ person }</p> */}
+
+        <p>{ 10 }</p>
+        <p>{ "hello, ninjas" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        {/* dynamic values as attributes in element tags */}
+        <a href={link}>Google Site</a>
+      </div>
     </div>
   );
 }
 
+// to import this in index.js
 export default App;
