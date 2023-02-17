8c5e043950c0f803a92568e85dbade245dd33488
nayana.v <nayana.v@compile.com>
2023-02-18 01:17:20 +0530

    multiple components: added Navbar.js and Home.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  // any valid js here
-  const title = 'New Blog' ;
-  const likes = 50;
-  //any type like array, etc except bool and object
-
-   // const person = { name: 'yoshi', age: 30 };
-   const link = 'http://www.google.com';
-
   return (
     <div className="App">
-      {/* className: camelcase */}
-      {/* not 'class' like in html
-      class is a keyword in JS, and this is basically a JS file.
-      babel converts className into class in html for the dom */}
-      <div className="content">
-        {/* <h1>App Component</h1> */}
-        <h1>WELCOME to { title }!</h1>
-        <p>Liked { likes } times</p>
 
-        {/* <p>{ person }</p> */}
+      <Navbar></Navbar>
+      {/* or <Navbar/> self closed*/}
 
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        {/* dynamic values as attributes in element tags */}
-        <a href={link}>Google Site</a>
-      </div>
+      <Home/>
     </div>
   );
 }
