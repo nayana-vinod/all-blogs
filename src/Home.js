66d4a0c07c590078146fdb04f0b3d02dcd1b8557
nayana.v <nayana.v@compile.com>
2023-03-05 20:00:52 +0530

    useEffect dependency

--- src/Home.js
+++ src/Home.js
@@ -8,29 +8,37 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
-    // can be used for fetching data
     // runs the function on every render of the component
     useEffect(()=>{
         console.log('use effect run')
-        // on changing the data, because of useState the component is re-rendered
-        // and useEffect is run on every re-render
+        console.log(name); 
+        // use depenedency array if run the fn for only first initial render
+    // }, []);
+    // second argument to useEffect() fn is an empty dependency array if no dependency
+    
+    },[name]);
+    // with name as a dependency => if state of name changed => render using setName => run the function on render
 
-        console.log(blogs);
-        // do not crud data here causes infinite loop because crud updates states => rerenders component => 
-        // runs useEffect again => crud data => updates state => rerender => useState again
-    })
+    // button multiple times wont change the state as the updated data is louis now, and hence no more updating to louis again
+    // ie useEffect will be run once only for the button to change name
 
     return ( 
         <div className="home">
-            {/* handleDelete function is passed as a prop to src/BlogList.js */}
             <BlogList blogs={ blogs } title="All Blogs" handleDelete={handleDelete}/>
+            <button onClick={() => setName('louis')}>change name</button>
+            <p>{ name }</p>
         </div>
      );
 }
  
 export default Home;
+
+// useEffect runs twice on refresh
+// must be first initial render and another render for idk why
